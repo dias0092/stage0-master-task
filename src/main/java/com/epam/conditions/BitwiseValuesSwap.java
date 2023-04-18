b4c3e010aaf38e7a58511f06b814169215788ceb
Dias Mukhametarkhim <54438346+dias0092@users.noreply.github.com>
2023-04-18 11:38:40 +0000

    do all tasks in Conditions

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,11 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        System.out.println("Before swap: first=" + first + ", second=" + second);
+        first ^= second;
+        second ^= first;
+        first ^= second;
+        System.out.println("After swap: first=" + first + ", second=" + second);
     }
 
 }
