7c0cd8c5cb6a10c6ee6bf6ceb3835e84fcd0b24a
Dias Mukhametarkhim <54438346+dias0092@users.noreply.github.com>
2023-04-18 11:35:04 +0000

    do task 1 and 2 in Lang Syntax

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,14 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
+    int reversed = 0;
     public void revert(int number) {
+        while(number !=0){
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /=10;
+        }
+        System.out.println(reversed);
     }
 
 }
